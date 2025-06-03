cf9eaf25355fe5c6623f369b5ded813440426c3c
polpo01 <marwanekasraoui@gmail.com>
2025-06-03 20:23:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -151,11 +151,12 @@ def suivi():
 
 # --- Lancer serveur ---
 
+
 if __name__ == "__main__":
-    port_env = os.environ.get("PORT")
     try:
-        port = int(port_env) if port_env and port_env.isdigit() else 5000
-    except ValueError:
+        port = int(os.environ.get("PORT", 5000))
+    except (ValueError, TypeError):
         port = 5000
 
     app.run(host="0.0.0.0", port=port)
+
